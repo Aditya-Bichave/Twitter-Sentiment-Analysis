1c0e1e3305e1255113ed8b4fcddf9e9fa9e87217
Aditya Bichave <35490238+Aditya-Bichave@users.noreply.github.com>
2019-04-21 22:30:25 +0530

    Updated Twitter Sentiment Analysis.py

--- Twitter Sentiment Analysis.py
+++ Twitter Sentiment Analysis.py
@@ -1,11 +1,11 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key = "PFSGrmCX41xCW3ffp8ZWCx3Kk"
-consumer_secret = "E6NVllPxwjYBTQnTa8x6N1YiEv0c1wJh4BGCFVy12EyQmxKc0L"
+consumer_key = ""
+consumer_secret = ""
 
-acess_token = "3680391439-BNPlqRhjK9umgA99iwQYYblpWCDkEtXJHIHhgmM"
-acess_token_secret = "IjVhD9yMBkQoI5qWHeg19tiSPd9ddTo7XXDmh2ejl5Fq5"
+acess_token = ""
+acess_token_secret = ""
 
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(acess_token,acess_token_secret)
